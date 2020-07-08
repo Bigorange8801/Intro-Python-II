65acafdc99d2fb7c60b232b24cd20d407532dac5
John <cjd8801@gmail.com>
2020-07-08 16:46:35 -0500

    player built

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Player :
+    def __init__(self,name,age,char):
+        self.name = name
+        self.age = age
+        self.char = char
+
+p1 = Player("Sonic", 29 , "Hedgehog")
+
+print(p1.name)
+print(p1.age)
+print(p1.char)
